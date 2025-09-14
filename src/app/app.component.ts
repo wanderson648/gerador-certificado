6da9c763de2796edc5a803a8e973c5d04a7aa44a
wanderson648 <testepedroca@gmail.com>
2025-09-14 15:41:24 -0300

    feat: adiciona component secundaryButton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { PrimaryButtonComponent } from './primary-button/primary-button.component';
+import { SecundaryButtonComponent } from "./secundary-button/secundary-button.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent],
+  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecundaryButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/secundary-button/secundary-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-secundary-button',
+  imports: [],
+  templateUrl: './secundary-button.component.html',
+  styleUrl: './secundary-button.component.css'
+})
+export class SecundaryButtonComponent {
+
+}
