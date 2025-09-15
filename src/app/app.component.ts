3d58d40d2f482ac5c323fc681387001f4744e4d4
wanderson648 <testepedroca@gmail.com>
2025-09-14 23:59:03 -0300

    feat: adiciona page certificados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
-import { PrimaryButtonComponent } from './primary-button/primary-button.component';
-import { SecundaryButtonComponent } from "./secundary-button/secundary-button.component";
-import { ItemCertificadoComponent } from "./item-certificado/item-certificado.component";
-import { BaseUiComponent } from "./base-ui/base-ui.component";
+import { PrimaryButtonComponent } from './_components/primary-button/primary-button.component';
+import { SecundaryButtonComponent } from "./_components/secundary-button/secundary-button.component";
+import { ItemCertificadoComponent } from "./_components/item-certificado/item-certificado.component";
+import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
+import { CertificadosComponent } from "./pages/certificados/certificados.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecundaryButtonComponent, ItemCertificadoComponent, BaseUiComponent],
+  imports: [RouterOutlet, NavbarComponent, BaseUiComponent, CertificadosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pages/certificados/certificados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SecundaryButtonComponent } from "../../_components/secundary-button/secundary-button.component";
+
+@Component({
+  selector: 'app-certificados',
+  imports: [SecundaryButtonComponent],
+  templateUrl: './certificados.component.html',
+  styleUrl: './certificados.component.css'
+})
+export class CertificadosComponent {
+
+}
