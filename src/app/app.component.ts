d06be75ddf346edb5764d583908345fc501e50a0
wanderson648 <testepedroca@gmail.com>
2025-09-14 23:46:30 -0300

    feat: adiciona component baseUi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { PrimaryButtonComponent } from './primary-button/primary-button.component';
 import { SecundaryButtonComponent } from "./secundary-button/secundary-button.component";
 import { ItemCertificadoComponent } from "./item-certificado/item-certificado.component";
+import { BaseUiComponent } from "./base-ui/base-ui.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecundaryButtonComponent, ItemCertificadoComponent],
+  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecundaryButtonComponent, ItemCertificadoComponent, BaseUiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
