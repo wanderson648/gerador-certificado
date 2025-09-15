3276572c580058cfdfeffcc0460e9fe685cd133e
wanderson648 <testepedroca@gmail.com>
2025-09-14 23:19:57 -0300

    feat: adiciona componente item de lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { PrimaryButtonComponent } from './primary-button/primary-button.component';
 import { SecundaryButtonComponent } from "./secundary-button/secundary-button.component";
+import { ItemCertificadoComponent } from "./item-certificado/item-certificado.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecundaryButtonComponent],
+  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecundaryButtonComponent, ItemCertificadoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/item-certificado/item-certificado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SecundaryButtonComponent } from "../secundary-button/secundary-button.component";
+
+@Component({
+  selector: 'app-item-certificado',
+  imports: [SecundaryButtonComponent],
+  templateUrl: './item-certificado.component.html',
+  styleUrl: './item-certificado.component.css'
+})
+export class ItemCertificadoComponent {
+
+}
