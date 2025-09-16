ce2eb25b87991501c8ef38831c5ed97e799aa0a3
wanderson648 <testepedroca@gmail.com>
2025-09-16 12:03:15 -0300

    feat: valida campos do forumulario

--- src/app/pages/certificados-form/certificados-form.component.ts
+++ src/app/pages/certificados-form/certificados-form.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponent } from '../../_components/primary-button/primary-button.component';
 import { SecundaryButtonComponent } from '../../_components/secundary-button/secundary-button.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-certificados-form',
   standalone: true,
-  imports: [PrimaryButtonComponent, SecundaryButtonComponent, FormsModule],
+  imports: [PrimaryButtonComponent, SecundaryButtonComponent, FormsModule, CommonModule],
   templateUrl: './certificados-form.component.html',
   styleUrl: './certificados-form.component.css'
 })
@@ -16,4 +17,8 @@ export class CertificadosFormComponent {
   nome: string = '';
   atividade: string = '';
   atividades: string[] = ['Angular', 'React', 'Vue'];
+
+  campoInvalido(control: NgModel) {
+    return control.invalid && control.touched;
+  }
 }
