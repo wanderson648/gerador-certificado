5f45785e2503dab04665698ffabbaf8db56d1cda
wanderson648 <testepedroca@gmail.com>
2025-09-16 11:13:06 -0300

    feat: adiciona NgModel no formulário

--- src/app/pages/certificados-form/certificados-form.component.ts
+++ src/app/pages/certificados-form/certificados-form.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponent } from '../../_components/primary-button/primary-button.component';
 import { SecundaryButtonComponent } from '../../_components/secundary-button/secundary-button.component';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-certificados-form',
   standalone: true,
-  imports: [PrimaryButtonComponent, SecundaryButtonComponent],
+  imports: [PrimaryButtonComponent, SecundaryButtonComponent, FormsModule],
   templateUrl: './certificados-form.component.html',
   styleUrl: './certificados-form.component.css'
 })
 export class CertificadosFormComponent {
 
+  nome: string = '';
+  atividade: string = '';
+  atividades: string[] = ['Angular', 'React', 'Vue'];
 }
