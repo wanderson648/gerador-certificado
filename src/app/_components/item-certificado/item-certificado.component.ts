4b9ab257c0f2e997456388a1ef2d74a230d22a59
wanderson648 <testepedroca@gmail.com>
2025-09-16 10:39:15 -0300

    feat: adiciona router para navegacao via typescript

--- src/app/_components/item-certificado/item-certificado.component.ts
+++ src/app/_components/item-certificado/item-certificado.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { SecundaryButtonComponent } from "../secundary-button/secundary-button.component";
-import { RouterLink } from '@angular/router';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-item-certificado',
-  imports: [SecundaryButtonComponent, RouterLink],
+  imports: [SecundaryButtonComponent],
   templateUrl: './item-certificado.component.html',
   styleUrl: './item-certificado.component.css'
 })
 export class ItemCertificadoComponent {
-  id: string = '6';
+  id: string = '2';
+
+  constructor(private router: Router) {}
+
+  redirecionaCertificado() {
+    this.router.navigate(['/certificados', 2]);
+  }
 }
