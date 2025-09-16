62f6eb5df2dcb095ff73c93f3a0e1c352d5241cb
wanderson648 <testepedroca@gmail.com>
2025-09-16 09:30:51 -0300

    feat: adiciona rotas no sistema

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
-import { CertificadoComponent } from "./pages/certificado/certificado.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, BaseUiComponent, CertificadoComponent],
+  imports: [RouterOutlet, NavbarComponent, BaseUiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/certificados/certificados.component.ts
+++ src/app/pages/certificados/certificados.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { SecundaryButtonComponent } from "../../_components/secundary-button/secundary-button.component";
 import { ItemCertificadoComponent } from "../../_components/item-certificado/item-certificado.component";
 
 @Component({
   selector: 'app-certificados',
-  imports: [SecundaryButtonComponent, ItemCertificadoComponent],
+  imports: [ItemCertificadoComponent],
   templateUrl: './certificados.component.html',
   styleUrl: './certificados.component.css'
 })
