67a0f93eaacec976d2f47a357ae8e2a828e8da4f
wanderson648 <testepedroca@gmail.com>
2025-09-16 10:25:15 -0300

    feat: adiciona routerlink

--- src/app/_components/item-certificado/item-certificado.component.ts
+++ src/app/_components/item-certificado/item-certificado.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { SecundaryButtonComponent } from "../secundary-button/secundary-button.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-item-certificado',
-  imports: [SecundaryButtonComponent],
+  imports: [SecundaryButtonComponent, RouterLink],
   templateUrl: './item-certificado.component.html',
   styleUrl: './item-certificado.component.css'
 })
 export class ItemCertificadoComponent {
-
+  id: string = '6';
 }
--- src/app/pages/certificado/certificado.component.ts
+++ src/app/pages/certificado/certificado.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SecundaryButtonComponent } from "../../_components/secundary-button/secundary-button.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-certificado',
-  imports: [SecundaryButtonComponent],
+  imports: [SecundaryButtonComponent, RouterLink],
   templateUrl: './certificado.component.html',
   styleUrl: './certificado.component.css'
 })
