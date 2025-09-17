aabee1817046fece44e8cdf42b7457619f2f4ed6
wanderson648 <testepedroca@gmail.com>
2025-09-17 00:14:01 -0300

    fix: ajutar rota com erro

--- src/app/_components/secundary-button/secundary-button.component.ts
+++ src/app/_components/secundary-button/secundary-button.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-secundary-button',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './secundary-button.component.html',
   styleUrl: './secundary-button.component.css'
 })
--- src/app/pages/certificados/certificados.component.ts
+++ src/app/pages/certificados/certificados.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit } from '@angular/core';
 import { ItemCertificadoComponent } from "../../_components/item-certificado/item-certificado.component";
 import { Certificado } from '../../interfaces/certificado';
 import { SecundaryButtonComponent } from "../../_components/secundary-button/secundary-button.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-certificados',
-  imports: [ItemCertificadoComponent, SecundaryButtonComponent],
+  imports: [ItemCertificadoComponent, SecundaryButtonComponent, RouterLink],
   templateUrl: './certificados.component.html',
   styleUrl: './certificados.component.css'
 })
